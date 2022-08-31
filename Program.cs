490a223cc481969b104afcf5cb5839a77a05c7e2
Anastasia <nastys11@gmail.com>
2022-08-31 15:23:44 +0300

    Добавление кода

--- Program.cs
+++ Program.cs
@@ -6,7 +6,27 @@ namespace DZseminar2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите первое число: ");
+            string first = Console.ReadLine();
+            int number1 = int.Parse(first);
+            Console.WriteLine("Введите второе число:");
+            string second = Console.ReadLine();
+            int number2 = int.Parse(second);
+            Console.WriteLine("Введите третье число:");
+            string three = Console.ReadLine();
+            int number3 = int.Parse(three);
+            if (number1>number2 && number1>number3)
+            {
+                 Console.WriteLine("Большее число: "+number1);
+            }
+            else if (number2>number1 && number2>number3)
+            {
+                 Console.WriteLine("Большее число: "+number2);
+            }
+            else if (number3>number1 && number3>number2)
+            {
+                 Console.WriteLine("Большее число: "+number3);
+            } 
         }
     }
 }
